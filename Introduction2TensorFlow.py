02751b1fc32ac641e8a1794ca5b4fc385155e70a
Audrey Galametz <37661757+agalametz@users.noreply.github.com>
2018-11-06 15:57:28 +0100

    Update Introduction2TensorFlow.py

--- Introduction2TensorFlow.py
+++ Introduction2TensorFlow.py
@@ -30,3 +30,16 @@ two = tf.constant(2, dtype=tf.int32)
 primes_doubled = primes * two
 print("primes_doubled:", primes_doubled)
 
+# Matrix multiplication:
+x = tf.constant([[5, 2, 4, 3], [5, 1, 6, -2], [-1, 3, -1, -2]], dtype=tf.int32)
+y = tf.constant([[2, 2], [3, 5], [4, 5], [1, 6]], dtype=tf.int32)
+matrix_multiply_result = tf.matmul(x, y)
+print(matrix_multiply_result)
+
+# Tensor Reshaping
+matrix = tf.constant([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14], [15, 16]], dtype=tf.int32)
+reshaped_2x8_matrix = tf.reshape(matrix, [2, 8])
+reshaped_4x4_matrix = tf.reshape(matrix, [4, 4])
+print("Original matrix (8x2):", matrix.numpy())
+print("Reshaped matrix (2x8):", reshaped_2x8_matrix.numpy())
+print("Reshaped matrix (4x4):", reshaped_4x4_matrix.numpy())
